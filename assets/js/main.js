4c87b976d470fa921ed2309fc90df220c076c019
Yasin <yasinauthor18@gmail.com>
2025-12-02 16:11:33 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+// main.js
+
+document.querySelectorAll('.tool-card').forEach(card => {
+    card.addEventListener('click', () => {
+        alert(`${card.textContent} tool will open here.`);
+    });
+});
 
